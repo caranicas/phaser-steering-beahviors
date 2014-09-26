c79da3fe070d845ba1761be9a826d5a02e94fd02
caranicas <kendrick.caranicas@gmail.com>
2014-09-26 15:05:30 -0700

    working wander

--- scripts/behaviors/behaviorFind.js
+++ scripts/behaviors/behaviorFind.js
@@ -12,7 +12,8 @@ BehaviorFind.prototype.constructor = BehaviorFind;
 BehaviorFind.prototype = {
 
 	update:function(){
-		var sVec = MovementUtils.seek(this.target.sprite.body.position,this.boid.sprite.body.position,this.boid.sprite.body.velocity, this.boid.maxSpeed, this.boid.maxForce)
+		var sVec = MovementUtils.seek(this.target.sprite.position,this.boid.sprite.position,this.boid.sprite.body.velocity, this.boid.maxSpeed, this.boid.maxForce)
+
 		this.boid.sprite.body.acceleration.add(sVec.x,sVec.y);
 		this.boid.sprite.body.velocity.add( this.boid.sprite.body.acceleration.x, this.boid.sprite.body.acceleration.y)
 		this.boid.sprite.body.acceleration.multiply(0,0);
--- scripts/objs/boid.js
+++ scripts/objs/boid.js
@@ -14,22 +14,9 @@ function Boid(game) {
 	this.maxForce = 1;
 	this.maxAvoid = 3;
 	this.maxSeeAhead = 100;
+
+
 	this.debugVel = null;
-
-	this.wanderCircleRad = 50;
-	this.wanderCircleDist = 100;
-	this.wanderAngle = 180;
-	this.wanderVariance = 40;
-	this.wanderDate = new Date();
-	this.wanderTime = 1;
-
-	this.wanderDebugVec = null;
-	this.wanderDebugCircle = null;
-	this.debugDisplacmentVec = null;
-
-	this.debugExtension = new Phaser.Point(0,0);
-	this.debugDisplacemt = new Phaser.Point(0,0);
-
 }
 
 // subclass extends superclass
@@ -38,32 +25,27 @@ Boid.prototype.constructor = Boid;
 
 
 
-Boid.prototype.create = function(pos,vel,angle, debug){
+Boid.prototype.create = function(pos,vel,angle, debug) {
 
-		//alert('BOID CREATE');
+	Entity.prototype.create.call(this, pos,vel,angle, debug);
+	if(debug)
+	{
+		this.debugVel = new Phaser.Line(0,0,0,0);
+	}
 
-		Entity.prototype.create.call(this, pos,vel,angle, debug);
-		if(debug)
-		{
-			this.debugVel = new Phaser.Line(0,0,0,0);
-		}
-
-		return this;
+	return this;
 }
 
-
-Boid.prototype.debugUpdate = function()
-{
+Boid.prototype.debugUpdate = function() {
 
 	Entity.prototype.debugUpdate.call(this);
 	this.debugVel.start.x = this.sprite.position.x;
 	this.debugVel.start.y = this.sprite.position.y;
 	this.debugVel.end.x = this.sprite.position.x + this.sprite.body.velocity.x;
 	this.debugVel.end.y = this.sprite.position.y + this.sprite.body.velocity.y;
-
 }
 
-Boid.prototype.debugRender = function(){
+Boid.prototype.debugRender = function() {
 
 	Entity.prototype.debugRender.call(this);
 	this.game.debug.geom(this.debugVel,'blue', true);
