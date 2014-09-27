64f92b6fd9f26888a750f9d7e4910636b4207db9
caranicas <kendrick.caranicas@gmail.com>
2014-09-26 17:01:34 -0700

    update the count

--- scripts/pages/flocking.js
+++ scripts/pages/flocking.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-	var numBoids = 5;
+	var numBoids = 40;
   var Flock = [];
   var BlackHoles = [];
   var Flockable = [];
