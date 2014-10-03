0c5e4469c93e2f4a0211e00380d0ad0b18d9bf96
caranicas <kendrick.caranicas@gmail.com>
2014-10-03 11:30:39 -0700

    evading working in principle

--- scripts/pages/evading.js
+++ scripts/pages/evading.js
@@ -11,55 +11,53 @@ $(document).ready(function() {
   function preload () {
   	game.load.image('spaceBG', 'assets/space.jpg');
   	game.load.image('ship1', 'assets/ship1.png');
-    game.load.image('ship2', 'assets/ship2.png');
+    game.load.image('ship3', 'assets/ship3.png');
 
   }
 
   function create () {
     var bg = game.add.sprite(game.world.centerX, game.world.centerY, 'spaceBG');
     bg.anchor.setTo(0.5, 0.5);
-   	createPursuingTest();
+   	createEvdaingTest();
 	}
 
-	function createPursuingTest()
-	{
-    var target = createWanderingTarget();
-    creatPursuingGroup(target);
-	}
-
-  function createWanderingTarget()
+	function createEvdaingTest()
   {
-    var boid = new WanderingShip(game);
-    boid.initalize(1,'ship2');
+    var preditor = createPursuingTarget();
+    var prey = creatEvadingObject();
+    preditor.updateTarget(prey);
+    prey.updateTarget(preditor);
+  }
+
+  function createPursuingTarget()
+  {
+    var boid = new PursuingShip(game,null);
+    boid.initalize(1,'ship1');
     var xpos = Math.floor(Math.random()*game.world.bounds.width);
     var ypos = Math.floor(Math.random()*game.world.bounds.height);
     var pos = new Phaser.Point(xpos,ypos);
     var vel = new Phaser.Point(0,0)
     boid.create(pos,vel, 0, isDebugging);
     boid.category = 1;
-    boid.behavior = new BehaviorWander(boid);
+    boid.behavior = new BehaviorPursue(boid);
     Flockable.push(boid);
     return boid;
   }
 
-  function creatPursuingGroup(target)
+  function creatEvadingObject()
   {
-    for(var i = 0; i < numBoids; ++i)
-    {
-      var boid = new PursuingShip(game, target);
-      boid.initalize(i,'ship1');
-      var xpos = Math.floor(Math.random()*game.world.bounds.width);
-      var ypos = Math.floor(Math.random()*game.world.bounds.height);
-       var pos = new Phaser.Point(xpos,ypos);
-      var vel = new Phaser.Point(0,-40)
-      boid.create(pos,vel, 0, isDebugging);
-      boid.category = 1;
-      boid.behavior = new BehaviorPursue(boid);
-      Flock.push(boid);
-      Flockable.push(boid);
-    }
+    var boid = new EvadingShip(game,null);
+    boid.initalize(1,'ship3');
+    var xpos = Math.floor(Math.random()*game.world.bounds.width);
+    var ypos = Math.floor(Math.random()*game.world.bounds.height);
+    var pos = new Phaser.Point(xpos,ypos);
+    var vel = new Phaser.Point(30,20)
+    boid.create(pos,vel, 0, isDebugging);
+    boid.category = 1;
+    boid.behavior = new BehaviorEvade(boid);
+    Flockable.push(boid);
+    return boid;
   }
-
    //LOOP
   function update(){
   	for (var i = 0; i < Flockable.length; i++)
